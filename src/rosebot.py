a525c49d2f6132edade708744e2e87f480b3baf7
Hannah Meisner <meisnehb@rose-hulman.edu>
2019-02-06 11:06:09 -0500

    raise and calibrate arm done

--- src/rosebot.py
+++ src/rosebot.py
@@ -192,6 +192,11 @@ class ArmAndClaw(object):
 
     def raise_arm(self):
         """ Raises the Arm until its touch sensor is pressed. """
+        self.motor.turn_on(100)
+        while True:
+            if self.touch_sensor.is_pressed():
+                self.motor.turn_off()
+                break
 
     def calibrate_arm(self):
         """
@@ -202,6 +207,13 @@ class ArmAndClaw(object):
                (i.e., 14.2 motor revolutions),
           3. Resets the motor's position to 0.
         """
+        self.raise_arm()
+        self.motor.turn_on(-100)
+        while True:
+            if self.motor.get_position() == 14.2:
+                self.motor.turn_off()
+                self.motor.reset_position()
+                break
 
     def move_arm_to_position(self, desired_arm_position):
         """
