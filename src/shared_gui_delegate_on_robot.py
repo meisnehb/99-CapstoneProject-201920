ca062ed972579c49e33cc8d1a38eeb2c23552b9a
Hannah Meisner <meisnehb@rose-hulman.edu>
2019-02-07 10:09:52 -0500

    worked on shared_gui_delegate

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -6,4 +6,145 @@
     and Hannah Meisner and Alyssa Taylor.
   Winter term, 2018-2019.
 """
+"""
+Using a Brickman (robot) as the receiver of messages.
+"""
 
+# Same as m2_fake_robot_as_mqtt_sender,
+# but have the robot really do the action.
+# Implement just FORWARD at speeds X and Y is enough.
+
+import mqtt_remote_method_calls as com
+import time
+import math
+import ev3dev.ev3 as ev3
+
+class Motor(object):
+    WheelCircumference = 1.3 * math.pi
+
+    def __init__(self, port, motor_type='wheel'):
+        # port must be 'A', 'B', 'C', or 'D'.  Use 'arm' as motor_type for Arm.
+        if motor_type == 'wheel':
+            self._motor = ev3.LargeMotor('out' + port)
+        else:
+            self._motor = ev3.MediumMotor('out' + port)
+
+    def turn_on(self, speed):  # speed must be -100 to 100
+        self._motor.run_direct(duty_cycle_sp=speed)
+
+    def turn_off(self):
+        self._motor.stop(stop_action="brake")
+
+    def get_position(self):  # Units are degrees (that the motor has rotated).
+        return self._motor.position
+
+    def reset_position(self):
+        self._motor.position = 0
+
+class DriveSystem(object):
+    """
+    Controls the robot's motion via GO and STOP methods,
+        along with various methods that GO/STOP under control of a sensor.
+    """
+
+    def __init__(self):
+        """
+        What comes in:  Two (optional) sensors.
+        What goes out:  Nothing, i.e., None.
+        Side effects:
+          -- Stores the (optional) sensors.
+          -- Constructs two Motors (for the left and right wheels).
+        Type hints:
+          :type color_sensor:              ColorSensor
+          :type infrared_proximity_sensor: InfraredProximitySensor
+        """
+        self.left_motor = Motor('B')
+        self.right_motor = Motor('C')
+
+
+    def go(self, left_wheel_speed, right_wheel_speed):
+        self.left_motor.turn_on(left_wheel_speed)
+        self.right_motor.turn_on(right_wheel_speed)
+
+    def stop(self):
+        self.left_motor.turn_off()
+        self.right_motor.turn_off()
+
+    def go_straight_for_seconds(self, seconds, speed):
+        start = time.time()
+        self.go(speed, speed)
+        # Note: using   time.sleep   to control the time to run is better.
+        # We do it with a WHILE loop here for pedagogical reasons.
+        while True:
+            if time.time() - start >= seconds:
+                self.stop()
+                break
+
+    def go_straight_for_inches_using_time(self, inches, speed):
+        # NOTE to students:  The constant and formula below are not accurate
+        seconds_per_inch_at_100 = 10.0  # 1 sec = 10 inches at 100 speed
+        seconds = abs(inches * seconds_per_inch_at_100 / speed)
+
+        self.go_straight_for_seconds(seconds, speed)
+
+    def go_straight_for_inches_using_sensor(self, inches, speed):
+        pass
+        self.left_motor.reset_position()
+        self.right_motor.reset_position()
+        inches_per_degree = self.left_motor.WheelCircumference / 360
+        position = 1 / (inches_per_degree / inches)
+
+        while(abs(self.left_motor.get_position()) < position):
+            self.left_motor.turn_on(speed)
+            self.right_motor.turn_on(speed)
+
+        self.left_motor.turn_off()
+        self.right_motor.turn_off()
+
+
+    def go_straight_until_black(self, speed):
+        """
+        Goes straight at the given speed until the robot is over
+        a black surface, as measured by the color sensor.
+        """
+        pass
+
+    def go_forward_until_distance_is_less_than(self, inches, speed):
+        """
+        Goes forward at the given speed until the distance
+        to the nearest object, per the infrared proximity sensor,
+        is less than the given number of inches.
+        """
+        pass
+
+    def tones_until_touch_sensor_is_pressed(self):
+        """
+        Plays an increasing sequence of short tones,
+        stopping when the touch sensor is pressed.
+        """
+        pass
+
+def main():
+    name1 = 'Robot09'
+    name2 = 'Hannah'
+
+    my_delegate = DelegateThatReceives()
+    mqtt_client = com.MqttClient(my_delegate)
+    mqtt_client.connect(name1, name2)
+    time.sleep(1)  # Time to allow the MQTT setup.
+    print()
+
+    while True:
+        time.sleep(0.01)  # Time to allow message processing
+
+class DelegateThatReceives(object):
+    def __init__(self):
+        self.drive = DriveSystem()
+
+    def forward(self, speedL, speedR):
+        self.drive.go(speedL, speedR)
+
+    def stop(self):
+        self.drive.stop()
+
+main()
